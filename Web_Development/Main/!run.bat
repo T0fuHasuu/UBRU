c2667eef91288bbfca5842aacd45f7dfae9f856b
T0fuHasuu <yuutdelta@gmail.com>
2025-01-29 14:52:27 +0700

    second refresh

--- Web_Development/Main/!run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-if defined VIRTUAL_ENV (deactivate) else (call ForStudy\Scripts\activate)
---
+++ Web_Development/Main/epl/data.py
@@ -0,0 +1,26 @@
+teams = [
+  {'id':1, 'name':'Arsenal'},
+  {'id':2, 'name':'Aston Villa'},
+  {'id':3, 'name':'Bornemouth'},
+  {'id':4, 'name':'Brentford'},
+  {'id':5, 'name':'Brighton & Hove Albion'},
+  {'id':6, 'name':'Chelsea'},
+  {'id':7, 'name':'Crystal Palace'},
+  {'id':8, 'name':'Everton'},
+  {'id':9, 'name':'Fulham'},
+  {'id':10, 'name':'Ipswich Town'},
+  {'id':11, 'name':'Leicester City'},
+  {'id':12, 'name':'Liverpool'},
+  {'id':13, 'name':'Manchester City'},
+  {'id':14, 'name':'Manchester United'},
+  {'id':15, 'name':'Newcastle United'},
+  {'id':16, 'name':'Nottingham Forest'},
+  {'id':17, 'name':'Southampton'},
+  {'id':18, 'name':'Tottenham Hotspur'},
+  {'id':19, 'name':'West Ham United'},
+  {'id':20, 'name':'Wolverhampton Wonderers'},
+]
+
+footballers = [
+  {'id':1, 'name': 'Trent Alexander-Arnold', 'position':'Defender', 'club':'Liverpool', 'nationality':'England', 'img_url':'https://resources.premierleague.com/premierleague/photos/players/250x250/p169187.png'}
+]
